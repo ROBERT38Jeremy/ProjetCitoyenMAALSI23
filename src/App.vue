869c2c73b632ae299286f32af2fadbd3f0adc8cf
JRobert <jeremy100401@gmail.com>
2024-11-04 15:53:14 +0100

    creation navbar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
-<script setup lang="ts">
-import { useDropZone } from '@vueuse/core';
-import { ref } from 'vue';
-
-const dropZoneRef = ref<HTMLDivElement>();
-
-function onDrop(files: File[] | null) {
-  // called when files are dropped on zone
-}
-
-const { isOverDropZone } = useDropZone(dropZoneRef, {
-  onDrop,
-  // specify the types of data to be received.
-  dataTypes: ['image/jpeg'],
-  // control multi-file drop
-  multiple: true,
-  // whether to prevent default behavior for unhandled events
-  preventDefaultForUnhandled: false,
-});
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TheNavBar from './components/TheNavBar.vue'
 </script>
 
 <template>
-  <div class="h-screen flex-col justify-center">
-    <p class="">
-      The HTML <code>&lt;p&gt;</code> element defines a paragraph. A
-      <span
-        ref="dropZoneRef"
-        class="border border-slate-400 rounded w-10 inline-block"
-      ></span>
-      paragraph always starts on a new line, and browsers automatically add some white
-      space (a margin) before and after a paragraph.
-    </p>
-  </div>
+  <header>
+    <TheNavBar />
+  </header>
+
+  <RouterView />
 </template>
+
+<style scoped>
+</style>
