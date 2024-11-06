3d79eff6110fe82ba0404a07a59ddbab7c2236f2
ImNotAOwl <siegel.matthieu@ymail.com>
2024-11-06 10:27:46 +0100

    test de rendu html

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/CoursOneView.vue'),
     },
+    {
+      path: '/atelier',
+      name: 'atelier',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AtelierView.vue'),
+    },
   ],
 });
 
---
+++ src/views/AtelierView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+const test = ref(`
+	<style>
+		.test, .test * {
+			all: revert
+		}
+		.one {
+			color: red;
+		}
+	</style>
+
+<div class="test">
+	<h1>Test de rendu</h1>
+	<img src="https://img.fotocommunity.com/portrait-de-tigre-b414cb5c-b28d-4e13-a956-c8503d730e77.jpg?height=1000" />
+	<p class="one">Toto avec la class one</p>
+</div>
+`);
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <div v-html="test"></div>
+  </div>
+</template>
+
+<style scoped></style>
