09bd5dc1e5cec6e4ae8036604761cd9edbaa84fd
Maxime COLLAS <maxime.collas11@yahoo.com>
2024-11-13 01:31:30 +0100

    Component to use Monaco in a page

---
+++ src/components/Monaco.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="editor">
+    </div>
+</template>
+
+<script setup>
+import { createEditor } from "@/composables/monaco-config.js"
+import { onMounted } from "vue";
+
+let editor;
+
+onMounted(() => {
+    editor = createEditor(document.getElementById('editor'), { language: 'html'})
+})
+</script>
+<style>
+#editor {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
