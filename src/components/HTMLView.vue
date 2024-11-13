11ccf3cbe718e0c433017a90fab7f98df4d524a8
Maxime COLLAS <maxime.collas11@yahoo.com>
2024-11-13 02:39:43 +0100

    add HTMLView component and use Monaco in it

---
+++ src/components/HTMLView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="IDE">
+        <div class="editor">
+            <Monaco />
+        </div>
+        <div v-html="htmlData.htmlDataValue"></div>
+    </div>
+</template>
+<script setup>
+import Monaco from '@/components/Monaco.vue';
+import usehtmlDataStore from "@/stores/htmlData.js";
+
+const htmlData = usehtmlDataStore();
+
+</script>
+<style scoped>
+.IDE {
+    display: flex;
+    flex-direction: row;
+}
+.editor {
+    width: 50%;
+}
+</style>
