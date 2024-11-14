88602bf4f978f7ffb8bd4a30a8dedc2e8cf59077
Maxime COLLAS <maxime.collas11@yahoo.com>
2024-11-14 23:53:30 +0100

    monaco auto compilation HTML

--- src/composables/monaco-config.js
+++ src/composables/monaco-config.js
@@ -38,6 +38,59 @@ function createEditor(targetElement, { language = 'html'}) {
     return monaco.editor.create(targetElement, monacoOptions);
 };
 
+// Enregistrement d'un fournisseur de complétion pour le langage HTML
+monaco.languages.registerCompletionItemProvider('html', {
+	provideCompletionItems: function(model, position) {
+	  // Définit des suggestions de balises HTML
+	  const suggestions = [
+		{
+		  label: 'html',
+		  kind: monaco.languages.CompletionItemKind.Keyword,
+		  insertText: '<html>\n\t$0\n</html>',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Balise HTML principale'
+		},
+		{
+		  label: 'head',
+		  kind: monaco.languages.CompletionItemKind.Keyword,
+		  insertText: '<head>\n\t$0\n</head>',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Contient les métadonnées du document'
+		},
+		{
+		  label: 'body',
+		  kind: monaco.languages.CompletionItemKind.Keyword,
+		  insertText: '<body>\n\t$0\n</body>',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Contient le contenu principal du document'
+		},
+		{
+		  label: 'div',
+		  kind: monaco.languages.CompletionItemKind.Keyword,
+		  insertText: '<div>$0</div>',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Conteneur de division'
+		},
+		{
+		  label: 'h1',
+		  kind: monaco.languages.CompletionItemKind.Keyword,
+		  insertText: '<h1>$0</h1>',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Titre de niveau 1'
+		},
+		{
+		  label: 'p',
+		  kind: monaco.languages.CompletionItemKind.Keyword,
+		  insertText: '<p>$0</p>',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Paragraphe'
+		}
+	  ];
+
+	  return { suggestions: suggestions };
+	}
+  });
+
 export {
     createEditor,
 }
