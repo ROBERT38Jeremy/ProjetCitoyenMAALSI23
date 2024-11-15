2ba82463d5c62a99bacccc9d484dc77991d643e3
Maxime COLLAS <maxime.collas11@yahoo.com>
2024-11-15 01:49:28 +0100

    add function to createModel and auto compilation for css

--- src/composables/monaco-config.js
+++ src/composables/monaco-config.js
@@ -38,6 +38,11 @@ function createEditor(targetElement, { language = 'html'}) {
     return monaco.editor.create(targetElement, monacoOptions);
 };
 
+	// model = un fichier dans la mémoire de monaco
+	  function createModel(data, language) {
+		return monaco.editor.createModel(data, language);
+	  }
+
 // Enregistrement d'un fournisseur de complétion pour le langage HTML
 monaco.languages.registerCompletionItemProvider('html', {
 	provideCompletionItems: function(model, position) {
@@ -91,6 +96,109 @@ monaco.languages.registerCompletionItemProvider('html', {
 	}
   });
 
+// Enregistrement d'un fournisseur de complétion pour le langage CSS
+monaco.languages.registerCompletionItemProvider('css', {
+	provideCompletionItems: function(model, position) {
+	  // Suggestions pour les propriétés CSS et les valeurs communes
+	  const suggestions = [
+		{
+		  label: 'color',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'color: ${1:#000000};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la couleur du texte',
+		},
+		{
+		  label: 'background-color',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'background-color: ${1:#ffffff};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la couleur de fond d’un élément',
+		},
+		{
+		  label: 'font-size',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'font-size: ${1:16px};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la taille de la police',
+		},
+		{
+		  label: 'margin',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'margin: ${1:0px};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la marge extérieure autour d’un élément',
+		},
+		{
+		  label: 'padding',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'padding: ${1:0px};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la marge intérieure (padding) d’un élément',
+		},
+		{
+		  label: 'display',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'display: ${1:block};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit le type de mise en page de l’élément',
+		},
+		{
+		  label: 'flex',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'flex: ${1:1};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la flexibilité d’un élément dans un conteneur flex',
+		},
+		{
+		  label: 'justify-content',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'justify-content: ${1:flex-start};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit l’alignement des éléments enfants sur l’axe principal',
+		},
+		{
+		  label: 'align-items',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'align-items: ${1:center};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit l’alignement des éléments enfants sur l’axe transversal',
+		},
+		{
+		  label: 'border',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'border: ${1:1px solid #000};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la bordure autour d’un élément',
+		},
+		{
+		  label: 'position',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'position: ${1:absolute};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit le type de positionnement de l’élément',
+		},
+		{
+		  label: 'top',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'top: ${1:0px};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit la position de l’élément depuis le haut du conteneur parent',
+		},
+		{
+		  label: 'transition',
+		  kind: monaco.languages.CompletionItemKind.Property,
+		  insertText: 'transition: ${1:all 0.3s ease};',
+		  insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+		  documentation: 'Définit les transitions d’animation sur les propriétés CSS',
+		}
+	  ];
+
+	  return { suggestions: suggestions };
+	}
+  });
+
 export {
     createEditor,
+	createModel,
 }
