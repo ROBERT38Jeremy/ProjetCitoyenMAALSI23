f9902021a58570efa15484c22d043c5d707f11ca
Maxime COLLAS <maxime.collas11@yahoo.com>
2024-11-15 01:47:25 +0100

    store htmlData add css and htmlCss computed

--- src/stores/htmlData.js
+++ src/stores/htmlData.js
@@ -1,14 +1,26 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 
 const usehtmlDataStore = defineStore('htmlData', () => {
-  const htmlDataValue = ref('')
+  const cssDataValue = ref('');
+  const htmlDataValue = ref('');
+  const htmlCss = computed(() => {
+    return `${htmlDataValue.value}<style scoped>${cssDataValue.value}</style>`
+  })
+
+  function updateCssDataValue(value) {
+    cssDataValue.value = value;
+  }
 
   function updatehtmlDataValue(value) {
     htmlDataValue.value = value;
   }
 
-  return { htmlDataValue, updatehtmlDataValue }
+  function getCodeHtmlAndCss() {
+    
+  }
+
+  return { cssDataValue, htmlDataValue, htmlCss, updateCssDataValue, updatehtmlDataValue }
 })
 
 export default usehtmlDataStore;
