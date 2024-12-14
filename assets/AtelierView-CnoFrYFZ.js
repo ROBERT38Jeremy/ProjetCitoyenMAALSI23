5aef4aea77d2d494c8432ddfb82f87da76b5c44e
Jérémy <jeremy100401@gmail.com>
2024-12-14 14:58:26 +0100

    Updates

---
+++ assets/AtelierView-CnoFrYFZ.js
@@ -0,0 +1,75 @@
+import{u as i,_ as a}from"./CodeView-XcWw7HbF.js";import{i as o,n,b as r}from"./index-BBgfVYPh.js";const s=`@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+}
+
+.main-container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: hsl(217, 19%, 35%);
+}
+
+.article-card {
+    max-width: 730px;
+    border-radius: 10px;
+}
+
+.img-box {
+    border-radius: 10px 10px 0 0;
+    overflow: hidden;
+    max-height: 300px;
+}
+
+.article-banner {
+    width: 100%;
+    height: 100%;
+}
+
+.article-content {
+    background: hsl(0, 0%, 100%);
+    padding: 32px 40px;
+    border-radius: 0 0 10px 10px;
+}
+
+.article-title {
+    font-size: 20px;
+    margin-bottom: 12px;
+    line-height: 1.4;
+}
+
+.article-text {
+    font-size: 13px;
+    color: hsl(214, 17%, 51%);
+    margin-bottom: 18px;
+    line-height: 1.5;
+}`,l=`<div class="main-container">
+    <div class="article-card">
+        <div class="img-box">
+            <img src="https://i.postimg.cc/Zq6vZ4D1/drawers.jpg" alt="banner" class="article-banner">
+        </div>
+
+        <div class="article-content">
+            <h3 class="article-title">Shift the overall look and feel by adding these wonderful touches to furniture
+                in your home</h3>
+
+            <p class="article-text">
+                Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninvinting.
+                I've got some simple tips to help you make any room feel complete
+            </p>
+
+            <div>
+                <h4>Michelle Doe</h4>
+
+                <div>17 Oct 2024</div>
+            </div>
+        </div>
+
+    </div>
+</div>`,g={__name:"AtelierView",setup(c){const{updateCssDataValue:e,updateHtmlDataValue:t}=i();return o(()=>{e(s),t(l)}),(d,p)=>(r(),n(a))}};export{g as default};
